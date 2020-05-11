1e287eefe1f6f4c2fa8796fa81903924ecc2157e
abdus salam <abdul1salam@hotmail.com>
2020-05-11 23:35:03 +0500

    ASP.NET MVC fundamentals

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,7 +15,61 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() {Name="Dilwaly" };
 
-            return View(movie);
+            var customers = new List<Customer>()
+            {
+
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+
+
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers= customers
+
+
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int movieid)
+        {
+
+            return Content("id =" + movieid);
+
+        } 
+
+        public ActionResult Index()
+        {
+
+            var movies = GetMovies();
+
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+            
+        }
+
+        
+
+        private IEnumerable<Movie> GetMovies()
+        {
+
+            return new List<Movie>
+            {
+
+            new Movie { Name="Dilwaly",Id=1},
+            new Movie { Name="DDJL",Id=2}
+
+
+            };
+
+
+
         }
 
 
